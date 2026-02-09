f6c77614578d8e713455f50bf52fff7ba1bdf465
tw93 <tw93@qq.com>
2026-02-09 10:15:39 +0800

    fix: optimize font rendering on macOS (use Light hinting)

--- config/src/font.rs
+++ config/src/font.rs
@@ -225,11 +225,10 @@ pub enum DisplayPixelGeometry {
     BGR,
 }
 
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, FromDynamic, ToDynamic)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, FromDynamic, ToDynamic)]
 pub enum FreeTypeLoadTarget {
     /// This corresponds to the default hinting algorithm, optimized
     /// for standard gray-level rendering.
-    #[default]
     Normal,
     /// A lighter hinting algorithm for non-monochrome modes. Many
     /// generated glyphs are more fuzzy but better resemble its
@@ -244,6 +243,16 @@ pub enum FreeTypeLoadTarget {
     HorizontalLcd,
     /// A variant of Normal optimized for vertically decimated LCD displays.
     VerticalLcd,
+}
+
+impl Default for FreeTypeLoadTarget {
+    fn default() -> Self {
+        if cfg!(target_os = "macos") {
+            Self::Light
+        } else {
+            Self::Normal
+        }
+    }
 }
 
 bitflags! {
